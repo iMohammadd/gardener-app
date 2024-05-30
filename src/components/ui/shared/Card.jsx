f816675ef65dbf71e19c0e1aebbb65c0d9f61487
Mohammad Mohammadi <imohammadd@gmail.com>
2024-05-30 19:49:41 +0330

    format cards value to show only 2 decimals

--- src/components/ui/shared/Card.jsx
+++ src/components/ui/shared/Card.jsx
@@ -1,7 +1,9 @@
+import { NumericFormat } from 'react-number-format'
+
 export default function Card({ icon, text, active = false, value = null, unit = null, has_alarm = false, alarming = null }) {
     return (
         <div className={`${active ? 'bg-cyan-800 text-white' : 'bg-white text-black'} ${has_alarm == true && (alarming ? 'shadow-red-500' : 'shadow-sky-600')} rounded py-6 flex flex-col items-center shadow-sm h-full place-content-center`}>
-            {value && (<span className=" font-extrabold text-4xl mb-4">{value} {unit}</span>)}
+            {value && (<span className=" font-bold text-4xl mb-4"><NumericFormat displayType='text' value={value} decimalScale={2} suffix={' ' + unit} /></span>)}
             <img src={icon} className=" w-[64px] h-[64px]" />
             <span className=" font-medium">{text}</span>
         </div>
